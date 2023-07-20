12f3fd1759602c825582220ecda3d3283fc9a691
R8nDr0p <rainchrstn@gmail.com>
2023-07-20 22:31:44 +0800

    added a link for the brand logo and fixed the responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import navLogo from "../assets/logo.svg";
 import "./navbar.css";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { BiSearchAlt } from "react-icons/bi";
 import { FaCircleXmark, FaBarsStaggered } from "react-icons/fa6";
 
@@ -27,23 +27,23 @@ function Navbar() {
       <div className="container mx-auto">
         <div className="row flex justify-between items-center">
           <div className="col flex items-center space-x-2">
-            <a href="">
+            <Link to={"/"} className="flex items-center">
               <img
                 src={navLogo}
                 alt="logo of the website"
-                className="h-[50px] bg-white"
+                className="max-h-20 bg-white"
               />
-            </a>
-            <h1 className="text-white">
-              Anime <span className="text-red-600">Hub</span>
-            </h1>
+              <h1 className="text-white text-3xl">
+                Anime <span className="text-red-600">Hub</span>
+              </h1>
+            </Link>
           </div>
           {/* web view */}
-          <div className="col w-96 sm:flex hidden space-x-1">
+          <div className="col w-96 sm:flex hidden space-x-1 justify-center">
             <input
               type="text"
               placeholder="Search here:"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded sm:w-[5.5rem] md:w-full focus:w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline transition-all duration-500"
             />
             <button className="border rounded py-2 px-3 ">
               <BiSearchAlt className="text-white text-xl" />
@@ -77,7 +77,7 @@ function Navbar() {
             </button>
           </div>
           {show && (
-            <div className="mobile sm:hidden absolute top-12 w-full z-20">
+            <div className="mobile sm:hidden absolute top-[5rem] w-full z-20">
               <ul className="flex flex-col space-y-4 bg-neutral-900 text-white p-4">
                 {tags.map((tag, index) => (
                   <NavLink
