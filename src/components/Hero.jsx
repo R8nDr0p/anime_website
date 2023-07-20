74868b556801122fb8ad8190db6c1af664cf7009
R8nDr0p <rainchrstn@gmail.com>
2023-07-20 18:42:09 +0800

    added hero component and style

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <section>
+      <div className="container mx-auto my-8">
+        <div className="row">
+          <div className="col">
+            <h2 className="text-center text-4xl font-[poppins]">
+              "Where Anime Dreams Come to Life: <br /> Your Ultimate Destination
+              for All Things Anime!"
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
