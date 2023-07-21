b53688072edc5208628b65461a10c7fb02ff584f
R8nDr0p <rainchrstn@gmail.com>
2023-07-21 17:51:07 +0800

    loading not needed and then made it responsive for mobile because of overflow

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,20 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Card from "./Card";
-import "./popular.css";
 
 function Popular() {
   const popularUrl = "https://api.jikan.moe/v4/top/anime";
 
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [PopularAnime, setPopularAnime] = useState([]);
+  const [popularAnime, setPopularAnime] = useState([]);
 
   useEffect(() => {
     fetch(popularUrl)
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", data);
-        setPopularAnime(data.data || []); // Assuming the 'data' object contains the actual array of popular anime
+        setPopularAnime(data.data || []);
         setLoading(false);
       })
       .catch((error) => {
@@ -25,34 +24,33 @@ function Popular() {
   }, []);
 
   useEffect(() => {
-    console.log("PopularAnime:", PopularAnime);
-  }, [PopularAnime]);
+    console.log("popularAnime:", popularAnime);
+  }, [popularAnime]);
 
   return (
     <section className="bg-neutral-900 p-5">
       <div className="container mx-auto">
-        <div className="row">
-          <div className="col text-center">
-            <h3 className="text-4xl mb-4 text-white">Popular Anime</h3>
-          </div>
+        <div className="text-center">
+          <h3 className="text-4xl mb-4 text-white">Popular Anime</h3>
         </div>
-      </div>
-      <div className="container mx-auto">
-        <div className="row flex justify-center flex-wrap gap-4">
-          {/* CARD COMPONENT */}
-          {PopularAnime.map((anime, index) => {
-            return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {loading ? (
+            <img
+              src="https://gifdb.com/images/high/wide-mouth-anime-girl-ct08b2zx2bsp84eg.gif"
+              alt=""
+            />
+          ) : (
+            popularAnime.map((anime, index) => (
               <Card
                 key={index}
                 title={anime.title}
                 synopsis={anime.synopsis}
                 image={anime.images.jpg.large_image_url}
-                loading={loading}
                 aired={anime.aired.string}
                 genres={anime.genres}
               />
-            );
-          })}
+            ))
+          )}
         </div>
       </div>
     </section>
