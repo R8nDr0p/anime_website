b116c765633f64ccf322d279cc59816eb85865a7
R8nDr0p <rainchrstn@gmail.com>
2023-07-21 18:20:14 +0800

    removed extra a-tags

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,17 +3,13 @@ import React from "react";
 function Card({ title, synopsis, image, aired, genres }) {
   return (
     <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <div className="flex justify-center">
-          <img className="rounded-t-lg mt-3 w-full" src={image} alt="" />
-        </div>
-      </a>
+      <div className="flex justify-center">
+        <img className="rounded-t-lg mt-3 w-full" src={image} alt="" />
+      </div>
       <div className="p-5">
-        <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {title}
-          </h5>
-        </a>
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {title}
+        </h5>
         <p className="font-normal text-gray-700 dark:text-gray-400 truncate">
           {synopsis}
         </p>
@@ -30,12 +26,12 @@ function Card({ title, synopsis, image, aired, genres }) {
             </span>
           ))}
         </p>
-        <a
+        <button
           href="#"
           className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
         >
           Read more
-        </a>
+        </button>
       </div>
     </div>
   );
