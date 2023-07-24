4510d3d786b08c20f36e0c8093b73991f1e16bd1
R8nDr0p <rainchrstn@gmail.com>
2023-07-24 18:44:35 +0800

    planning to add button on my home.jsx page for some interaction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,17 @@ function Hero() {
   return (
     <section>
       <div className="bg-container py-[8rem] bg-right sm:bg-right-top">
-        <h2 className="text-3xl text-center text-white">
-          "Where Anime Dreams Come to Life: <br /> Your Ultimate Destination for
-          All Things Anime!"
-        </h2>
+        <div className="row">
+          <div className="col">
+            <h2 className="text-3xl sm:text-6xl text-center text-white">
+              "Where Anime Dreams Come to Life: <br /> Your Ultimate Destination
+              for All Things Anime!"
+            </h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col"></div>
+        </div>
       </div>
     </section>
   );
