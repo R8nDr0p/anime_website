c6563fbafc16b47ef39817c4f90e6141613f9ecd
R8nDr0p <rainchrstn@gmail.com>
2023-08-17 11:29:54 +0800

    made a simple about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section className="dark:bg-neutral-900 pt-10">
+      <div className="contaner mx-auto">
+        <div className="row">
+          <div className="col">
+            <h2 className="text-center text-3xl text-black dark:text-white">
+              About
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
