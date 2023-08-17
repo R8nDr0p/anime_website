6b1d0961116fd67a78284b750bcc8ef9e9702e6b
R8nDr0p <rainchrstn@gmail.com>
2023-08-17 11:26:39 +0800

    added a layout component

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import About from "./pages/About";
 import Contacts from "./pages/Contacts";
 import Home from "./pages/Home";
 import AnimeInfoPage from "./pages/AnimeInfoPage";
+import Layout from "./pages/Layout";
+import Results from "./pages/Results";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Navbar />
+        {/* <Navbar /> */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contacts />} />
-          <Route path="anime-info/:id" element={<AnimeInfoPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contacts />} />
+            <Route path="anime-info/:id" element={<AnimeInfoPage />} />
+            <Route path="results/:search_term" element={<Results />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
